e27a5cf5fa0bfc8f66a42a7222fe0c1bc82744b2
Prajwal K <154422749+Prajxl@users.noreply.github.com>
2026-02-24 11:51:46 +0530

    Array

---
+++ src/MODULE1/ARRAYS/P2.java
@@ -0,0 +1,29 @@
+package MODULE1.ARRAYS;
+
+public class P2 {
+    public static void main(String[] args) {
+
+        // Double Array Declaration
+
+        double[] dArr={8.2,8.3,8.5,9.5};
+        for (int i = 0; i < dArr.length; i++) {
+            System.out.println(dArr[i]);
+        }
+
+
+        // Float Array Declaration
+
+        float[] ft = new float[3];
+        ft[0]=1.3f;
+        ft[1]=2.3f;
+        ft[2]=4.5f;
+        for (int i = 0; i < ft.length; i++) {
+            System.out.println(ft[i]);
+        }
+
+        char[] ch = {'J','S','p','i','d'};
+        for (int i = 0; i < ch.length; i++) {
+            System.out.println(ch[i]);
+        }
+    }
+}
---
+++ src/MODULE1/ARRAYS/Program1.java
@@ -0,0 +1,22 @@
+package MODULE1.ARRAYS;
+
+import java.util.Scanner;
+
+public class Program1 {
+    public static void main(String[] args) {
+        int[] arr=new int[4];
+//        Scanner sc=new Scanner(System.in);
+//        for (int i = 0; i < arr.length; i++) {
+//            arr[i]= sc.nextInt();
+//        }
+        arr[0]=12;
+        arr[1]=13;
+        arr[2]=14;
+        arr[3]=15;
+
+        for (int i=0;i<arr.length;i++)
+        {
+            System.out.println(arr[i]);
+        }
+    }
+}
