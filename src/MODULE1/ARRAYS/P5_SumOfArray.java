d83a8c19242a4edb188630eb197a6c540720bd74
Prajwal K <154422749+Prajxl@users.noreply.github.com>
2026-02-25 10:49:16 +0530

    Sum of Array

---
+++ src/MODULE1/ARRAYS/P5_SumOfArray.java
@@ -0,0 +1,13 @@
+package MODULE1.ARRAYS;
+
+public class P5_SumOfArray {
+    public static void main(String[] args) {
+        int sum=0;
+        int[] arr={12,10,13,14,15};
+        for(int i=0;i< arr.length;i++)
+        {
+            sum=sum+arr[i];
+        }
+        System.out.println("Array sum "+sum);
+    }
+}
