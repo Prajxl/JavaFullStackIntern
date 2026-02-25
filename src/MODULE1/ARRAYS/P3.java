443cdfc16991e42f60f8be01478fb6f397345a43
Prajwal K <154422749+Prajxl@users.noreply.github.com>
2026-02-25 10:37:41 +0530

    odd and even from array

---
+++ src/MODULE1/ARRAYS/P3.java
@@ -0,0 +1,16 @@
+package MODULE1.ARRAYS;
+
+public class P3 {
+    public static void main(String[] args) {
+        int[] arr={12,14,15,13,17,16};
+        for (int i=0;i<arr.length;i++)
+        {
+
+            if(arr[i]%2!=0)   // Odd Number = if(arr[i]%2!=0)
+            {
+                System.out.println(arr[i]);
+            }
+        }
+
+    }
+}
