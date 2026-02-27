e15d24e5bd9b078445954302915affa826a923e8
Prajwal K <154422749+Prajxl@users.noreply.github.com>
2026-02-27 10:47:19 +0530

    UpperCase and LowerCase

---
+++ src/MODULE1/ARRAYS/toUpperCase_toLowerCase.java
@@ -0,0 +1,10 @@
+package MODULE1.ARRAYS;
+
+public class toUpperCase_toLowerCase {
+    public static void main(String[] args) {
+        String str="Java_SE25";
+        System.out.println(str.toUpperCase());
+        System.out.println(str.toLowerCase());
+        System.out.println(str);
+    }
+}
