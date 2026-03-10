ea09113ad6eb412753dfca9d5e65b5510e48506f
Prajwal K <154422749+Prajxl@users.noreply.github.com>
2026-03-10 21:48:55 +0530

    TicketCounter Application

--- src/MODULE2/Constructors/MainClass5.java
+++ src/MODULE2/Constructors/MainClass5.java
@@ -1,30 +1,57 @@
 package MODULE2.Constructors;
 
-class TicketCounter{
+import java.util.Scanner;
+
+class  TicketCounter{
     int counterNumber;
     int numberOfTickets;
-    String ticket_Officer_Name;
+    String ticketOfficerName;
+    static int sumOfTicket=0;
 
-    TicketCounter(int counterNumber,int numberOfTickets,String ticket_Officer_Name)
-    {
-        this.counterNumber=counterNumber;
-        this.numberOfTickets=numberOfTickets;
-        this.ticket_Officer_Name=ticket_Officer_Name;
-        System.out.println("Counter Number= "+counterNumber+" "+"Number of Ticker= "+numberOfTickets+" Ticket Officer Name= "+ticket_Officer_Name);
+    TicketCounter(int counterNumber,int numberOfTickets,String ticketOfficerName) {
+        this.counterNumber = counterNumber;
+        this.numberOfTickets = numberOfTickets;
+        this.ticketOfficerName = ticketOfficerName;
+        sumOfTicket += numberOfTickets;
     }
 
-    void display()
+    void details()
     {
-        System.out.println("Counter Number= "+counterNumber+" "+"Number of Ticker= "+numberOfTickets+" Ticket Officer Name= "+ticket_Officer_Name);
+        System.out.println("Counter Number= "+counterNumber+"\n"+"Number of Ticker= "+numberOfTickets+"\nTicket Officer Name= "+ ticketOfficerName);
+        System.out.println("There are "+numberOfTickets+" Available at counter "+counterNumber);
     }
+
+    static void availableTicket()
+    {
+        System.out.println("There are Total = "+  sumOfTicket +" Available");
+    }
+
+    void buyTickets(Scanner sc)
+    {
+        System.out.print("Enter the Number of ticket needed = ");
+        int n=sc.nextInt();
+        if(sumOfTicket>=n)
+        {
+            System.out.println("Buying "+n+" tickets, success");
+            sumOfTicket = sumOfTicket - n;
+            System.out.println("Available Ticket = "+sumOfTicket);
+        }else
+        {
+            System.out.println("Buying "+n+" tickets, Failed, due to insufficient ticekts in counter"+counterNumber);
+        }
+    }
+
 
 }
 
 public class MainClass5 {
     public static void main(String[] args) {
-        TicketCounter t1 = new TicketCounter(1,100,"Prajwal");
-        t1.display();
-        TicketCounter t2 = new TicketCounter(2,95,"nandan");
-        t2.display();
+        Scanner sc= new Scanner(System.in);
+        TicketCounter t1 = new TicketCounter(1,10,"Prajwal");
+        t1.details();
+        TicketCounter t2 = new TicketCounter(2,9,"nandan");
+        t2.details();
+        TicketCounter.availableTicket();
+        t1.buyTickets(sc);
     }
 }
